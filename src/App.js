6e197ca62e675df51e74bc89175a767b1ae9da3b
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2022-11-12 19:34:30 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import React, { useState } from 'react';
+import Home from './components/Home';
+import Food from './components/Food';
+import Wines from './components/Wines';
+import Foxy from './components/Foxy';
+import Gallery from './components/Gallery';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  const [currentTab, setCurrentTab] = useState("home");
+
+  const renderTab = () => {
+    switch (currentTab) {
+      case "food":
+        return <Food  />;
+
+      case "home":
+        return <Home />;
+      
+      case "blog":
+        return <blog />;
+
+        case "wines":
+          return <Wines />;
+          
+          case "foxy":
+            return <Foxy />;
+          
+            case "gallery":
+              return <Gallery />;  
+
+
+
+      default:
+        return null;
+    }
+  };
+  return (
+
+    <div>
+      <div>
+
+        <Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
+      </div>
+
+      <div>
+        <main>{renderTab()}
+        
+        </main>
+        
+      </div>
+
+      <div>
+        <Footer></Footer>
+      </div>
+    </div>
+
+  );
+}
+
+
+export default App;
---
+++ src/components/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Home() {
+
+    return (
+            <section className="home">
+                <h2> who are we?</h2>
+                    <p>what's up people</p>
+    
+            </section>
+   ) 
+}
+
+export default Home;
